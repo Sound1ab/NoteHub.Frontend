b133270a5ce5064714609c8e56bb3d779108f777
Phillip Parker <phillip-parker@outlook.com>
2019-07-03 17:32:49 +0100

    feat(app/toolbar): improve dropdown menus in toolbar

--- app/src/components/molecules/Toolbar/Toolbar.tsx
+++ app/src/components/molecules/Toolbar/Toolbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Popover from 'react-awesome-popover'
+import Popup from 'reactjs-popup'
 import { CreateFileModal, DeleteFileModal, DeleteRepoModal } from '..'
 import { COLOR } from '../../../enums'
 import { useStore } from '../../../hooks'
@@ -28,22 +28,23 @@ const Style = styled.div`
     margin-left: auto;
   }
 
-  .Toolbar-tooltip {
-    padding: ${({ theme }) => theme.spacing.xs};
-    background-color: ${({ theme }) => theme.colors.background.tertiary};
-    box-shadow: 0 1px 4px -1px rgba(0, 0, 0, 0.2);
+  .Toolbar-tooltip-content {
+    padding: ${({ theme }) => theme.spacing.xs}!important;
+    background-color: ${({ theme }) =>
+      theme.colors.background.tertiary}!important;
+    box-shadow: 0 1px 4px -1px rgba(0, 0, 0, 0.2) !important;
+    border: none !important;
 
-    &__left {
-      margin-left: ${({ theme }) => theme.spacing.xxs};
-    }
-
-    &__right {
-      margin-right: ${({ theme }) => theme.spacing.xxs};
-    }
-
-    & > * + * {
+    & > button + button {
       margin-top: ${({ theme }) => theme.spacing.xs};
     }
+  }
+
+  .Toolbar-tooltip-arrow {
+    background-color: ${({ theme }) =>
+      theme.colors.background.tertiary}!important;
+    border: none !important;
+    box-shadow: none !important;
   }
 `
 
@@ -55,26 +56,29 @@ export function Toolbar() {
 
   return (
     <Style>
-      <Popover placement="right-start">
-        <button>
-          {state.repo.activeRepo.name && (
-            <Heading color={COLOR.INHERIT} type="h4">
-              {state.repo.activeRepo.name}
-            </Heading>
-          )}
-        </button>
-        <div className="Toolbar-tooltip Toolbar-tooltip__left">
-          <button
-            className="Toolbar-option"
-            onClick={setIsDeleteRepoModalOpen.bind(null, true)}
-          >
-            <Icon icon="trash" prefix="fa" size="sm" marginRight />
-            <Heading color={COLOR.INHERIT} type="h5">
-              Delete Repo
-            </Heading>
+      <Popup
+        trigger={
+          <button>
+            {state.repo.activeRepo.name && (
+              <Heading color={COLOR.INHERIT} type="h4">
+                {state.repo.activeRepo.name}
+              </Heading>
+            )}
           </button>
-        </div>
-      </Popover>
+        }
+        position="bottom left"
+        className="Toolbar-tooltip"
+      >
+        <button
+          className="Toolbar-option"
+          onClick={setIsDeleteRepoModalOpen.bind(null, true)}
+        >
+          <Icon icon="trash" prefix="fa" size="sm" marginRight />
+          <Heading color={COLOR.INHERIT} type="h5">
+            Delete Repo
+          </Heading>
+        </button>
+      </Popup>
       <div
         className="Toolbar-new-note"
         onClick={setIsCreateFileModalOpen.bind(null, true)}
@@ -94,12 +98,16 @@ export function Toolbar() {
         size="lg"
         color={COLOR.MEDIUM}
       />
-
-      <Popover placement="right-start">
-        <button>
-          <Icon icon="ellipsis-h" prefix="fa" size="lg" marginLeft />
-        </button>
-        <div className="Toolbar-tooltip Toolbar-tooltip__right">
+      <Popup
+        trigger={
+          <button>
+            <Icon icon="ellipsis-h" prefix="fa" size="lg" marginLeft />
+          </button>
+        }
+        position="bottom right"
+        className="Toolbar-tooltip"
+      >
+        <>
           <button
             className="Toolbar-option"
             onClick={setIsDeleteModalOpen.bind(null, true)}
@@ -109,19 +117,26 @@ export function Toolbar() {
               Delete File
             </Heading>
           </button>
-          <a
-            className="Toolbar-option"
-            href={state.repo.activeFile._links.html}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon icon="external-link-alt" prefix="fa" size="sm" marginRight />
-            <Heading color={COLOR.INHERIT} type="h5">
-              View on Github
-            </Heading>
-          </a>
-        </div>
-      </Popover>
+          <button>
+            <a
+              className="Toolbar-option"
+              href={state.repo.activeFile._links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                icon="external-link-alt"
+                prefix="fa"
+                size="sm"
+                marginRight
+              />
+              <Heading color={COLOR.INHERIT} type="h5">
+                View on Github
+              </Heading>
+            </a>
+          </button>
+        </>
+      </Popup>
       <CreateFileModal
         isOpen={isCreateFileModalOpen}
         onRequestClose={setIsCreateFileModalOpen.bind(null, false)}
