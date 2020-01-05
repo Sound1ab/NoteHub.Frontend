ff224fe9743f7c61da4f908e612a7d249f288f81
Dave <dave@Daves-MacBook-Pro.local>
2020-01-05 19:58:50 +0000

    fix(repo): remove files from cache when deleting repo

--- src/hooks/Repo/useDeleteRepo.ts
+++ src/hooks/Repo/useDeleteRepo.ts
@@ -1,5 +1,6 @@
-import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
 import {
   DeleteRepoMutation,
   DeleteRepoMutationVariables,
@@ -7,8 +8,8 @@ import {
   ListReposQueryVariables,
 } from '../../components/apollo/generated_components_typings'
 import { RepoFragment } from '../../fragments'
-import { useReadGithubUser } from '..'
 import { ListReposDocument } from './useListRepos'
+import { useReadGithubUser } from '..'
 
 export const DeleteRepoDocument = gql`
   ${RepoFragment}
@@ -53,6 +54,29 @@ export function useDeleteRepo() {
             username: user.login,
           },
         })
+
+        function deleteFileKeysFromCache() {
+          if (!deletedRepo || !user) return
+
+          const listFilesVariables = {
+            repo: deletedRepo.name,
+            username: user.login,
+          }
+
+          const key = `\\$ROOT_QUERY.listFiles\\(${JSON.stringify(
+            listFilesVariables
+          )}\\)`
+
+          const regex = new RegExp(key, 'gi')
+
+          Object.keys((cache as any).data.data).forEach(key => {
+            if (key.match(regex)) {
+              ;(cache as any).data.delete(key)
+            }
+          })
+        }
+
+        deleteFileKeysFromCache()
       },
     }
   )
