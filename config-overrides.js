60ead88b0669bd5d98b9fb232dd029ea23ce429e
sound1ab <phillip-parker@outlook.com>
2020-09-27 13:18:55 +0100

    Remove rewired config overrides

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { override, useEslintRc } = require('customize-cra')
-
-// eslint-disable-next-line no-undef
-module.exports = override(useEslintRc())
