9d34253981bc20250ad48fb7f269bd69641a2a86
sound1ab <phillip-parker@outlook.com>
2020-12-12 11:31:17 +0000

    Refactor context menu  dropdown into hook

--- src/components/templates/Dashboard/Editor/ContextMenu/ContextMenu.tsx
+++ src/components/templates/Dashboard/Editor/ContextMenu/ContextMenu.tsx
@@ -1,12 +1,6 @@
 import React, { RefObject } from 'react'
 
-import {
-  useContextMenu,
-  useDropzone,
-  useCodeMirror,
-  useReadCurrentPath,
-} from '../../../../../hooks'
-import { isFile } from '../../../../../utils'
+import { useContextMenu, useContextMenuDropdown } from '../../../../../hooks'
 import { Fade } from '../../../../animation'
 import { Dropdown } from '../../../../atoms'
 
@@ -15,95 +9,12 @@ interface IContextMenu {
 }
 
 export function ContextMenu({ targetRef }: IContextMenu) {
-  const currentPath = useReadCurrentPath()
-  const isMarkdownEditorActive = isFile(currentPath)
   const { isOpen, Portal, ref, setOpen } = useContextMenu(targetRef)
-  const { actions } = useCodeMirror()
-  const { openFileDialog, Dropzone } = useDropzone()
+  const { items, Dropzone } = useContextMenuDropdown()
 
-  if (!actions) {
+  if (!items) {
     return null
   }
-
-  const {
-    toggleOrderedList,
-    toggleCodeBlock,
-    toggleUnorderedList,
-    toggleItalic,
-    toggleBold,
-    toggleBlockquote,
-    drawHorizontalRule,
-    drawLink,
-    drawTable,
-  } = actions
-
-  const items = [
-    {
-      heading: 'Text',
-      onClick: toggleItalic,
-      title: 'Italic',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'italic' as const,
-    },
-    {
-      onClick: toggleBold,
-      title: 'Bold',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'bold' as const,
-    },
-    {
-      onClick: toggleBlockquote,
-      title: 'Quote',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'quote-right' as const,
-      hasSeparator: true,
-    },
-    {
-      heading: 'List',
-      onClick: toggleOrderedList,
-      title: 'Ordered',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'list-ol' as const,
-    },
-    {
-      onClick: toggleUnorderedList,
-      title: 'Unordered',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'list' as const,
-      hasSeparator: true,
-    },
-    {
-      heading: 'Insert',
-      onClick: toggleCodeBlock,
-      title: 'Code block',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'code' as const,
-    },
-    {
-      onClick: drawHorizontalRule,
-      title: 'Horizontal line',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'minus' as const,
-    },
-    {
-      onClick: drawTable,
-      title: 'Table',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'table' as const,
-    },
-    {
-      onClick: drawLink,
-      title: 'Link',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'link' as const,
-    },
-    {
-      onClick: openFileDialog,
-      title: 'Image',
-      isDisabled: !isMarkdownEditorActive,
-      icon: 'image' as const,
-    },
-  ]
 
   return (
     <>
---
+++ src/hooks/dropdown/useContextMenuDropdown.tsx
@@ -0,0 +1,81 @@
+import { isFile } from '../../utils'
+import { useCodeMirror, useDropzone, useReadCurrentPath } from '..'
+
+export function useContextMenuDropdown() {
+  const currentPath = useReadCurrentPath()
+  const { Dropzone, openFileDialog } = useDropzone()
+  const isMarkdownEditorActive = isFile(currentPath)
+  const { actions } = useCodeMirror()
+
+  const items = actions
+    ? [
+        {
+          heading: 'Text',
+          onClick: actions.toggleItalic,
+          title: 'Italic',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'italic' as const,
+        },
+        {
+          onClick: actions.toggleBold,
+          title: 'Bold',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'bold' as const,
+        },
+        {
+          onClick: actions.toggleBlockquote,
+          title: 'Quote',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'quote-right' as const,
+          hasSeparator: true,
+        },
+        {
+          heading: 'List',
+          onClick: actions.toggleOrderedList,
+          title: 'Ordered',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'list-ol' as const,
+        },
+        {
+          onClick: actions.toggleUnorderedList,
+          title: 'Unordered',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'list' as const,
+          hasSeparator: true,
+        },
+        {
+          heading: 'Insert',
+          onClick: actions.toggleCodeBlock,
+          title: 'Code block',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'code' as const,
+        },
+        {
+          onClick: actions.drawHorizontalRule,
+          title: 'Horizontal line',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'minus' as const,
+        },
+        {
+          onClick: actions.drawTable,
+          title: 'Table',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'table' as const,
+        },
+        {
+          onClick: actions.drawLink,
+          title: 'Link',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'link' as const,
+        },
+        {
+          onClick: openFileDialog,
+          title: 'Image',
+          isDisabled: !isMarkdownEditorActive,
+          icon: 'image' as const,
+        },
+      ]
+    : null
+
+  return { items, Dropzone }
+}
