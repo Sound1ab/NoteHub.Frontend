93e84d95120618ab55f0522f9fedc39f58001487
sound1ab <phillip-parker@outlook.com>
2021-12-28 09:40:21 +0000

    Fix eslint error in js file

--- src/services/GitFetchClient/utils/forAwait.js
+++ src/services/GitFetchClient/utils/forAwait.js
@@ -3,6 +3,7 @@ import { getIterator } from './getIterator.js'
 // Currently 'for await' upsets my linters.
 export async function forAwait(iterable, cb) {
   const iter = getIterator(iterable)
+  // eslint-disable-next-line no-constant-condition
   while (true) {
     const { value, done } = await iter.next()
     if (value) await cb(value)
